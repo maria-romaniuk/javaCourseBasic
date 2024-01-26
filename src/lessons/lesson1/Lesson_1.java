cb0a721d5d683966a36987759c427e38fe94d763
maria-romaniuk <romaniuk.mary@gmail.com>
2024-01-26 16:52:50 +0100

    deleted packages

--- src/lessons/lesson1/Lesson_1.java
+++
@@ -1,4 +0,0 @@
-package lessons.lesson1;
-
-public class Lesson_1 {
-}
--- src/lessons/lesson3/Lesson_3.java
+++
@@ -1,8 +0,0 @@
-package lessons.lesson3;
-
-public class Lesson_3 {
-    public static void main(String[] args) {
-    }
-
-
-}
